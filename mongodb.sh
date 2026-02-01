9f445472b34a319407cb6c1d9d47e07172d49606
chandana <allu.chandu05@gmail.com>
2026-01-31 22:37:07 -0600

    actions

--- mongodb.sh
+++ mongodb.sh
@@ -26,16 +26,16 @@ VALIDATE(){
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying Mongo Repo" 
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y  &>>$LOGS_FILE
 VALIDATE $? "Installing MongoDB Server" 
 
-systemctl enable mongod 
+systemctl enable mongod &>>$LOGS_FILE
 VALIDATE $? "Enable MongoDB" 
 
 systemctl start mongod
 VALIDATE $? "Start MongoDB"
 
-sed -i 's/127.0.0.0/0.0.0.0/g' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections"
 
 systemctl restart mongod
